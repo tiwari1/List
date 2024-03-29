61647401ceafda7b98dc033434aa2de12699f035
Akshita Tiwari <85664297+tiwari1@users.noreply.github.com>
2024-03-29 16:07:05 +0530

    Add files via upload

---
+++ List.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# List
+# []
+# mutable
+# diffrnt diffrnt datatypes values stores at a time
+
+
+# In[1]:
+
+
+l=[1,2.3,'python',True]
+print(type(l))
+print(l)
+
+
+# In[ ]:
+
+
+# List functions
+1. append()
+2. extend()
+3. insert()
+4. pop()
+5. sort()
+6. remove()
+7. reverse()
+8. count()
+9. index()
+
+
+# In[3]:
+
+
+# append() # it takes one argument
+l=[1,2.3,'python',True]
+l.append('hello')
+l
+
+
+# In[4]:
+
+
+# append() # it takes one argument
+l=[1,2.3,'python',True]
+l.append([['hello','hii']])
+l
+
+
+# In[5]:
+
+
+# extend() # it takes one argument
+l=[1,2.3,'python',True]
+l.extend([['hello','hii']])
+l
+
+
+# In[7]:
+
+
+# extend() # it takes one argument
+l=[1,2.3,'python',True]
+l.extend('hello')
+l
+
+
+# In[ ]:
+
+
+
+
